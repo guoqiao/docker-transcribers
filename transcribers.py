59e005666990f7abd9e1d11775fa79eb2a471c8b
guoqiao <guoqiao@gmail.com>
2026-01-02 15:11:08 +1300

    transcribers.py: load -> _load

--- transcribers.py
+++ transcribers.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
 """ASR: Automatic Speech Recognition."""
 
+from contextlib import contextmanager
+from functools import wraps
 import json
 import os
+import shlex
+import subprocess
 import time
 from pathlib import Path
 
@@ -19,16 +23,97 @@ def sec2ts(t: float|str) -> str:
     return f"{h:02d}:{m:02d}:{s:02d}.{ms:03d}"
 
 
+class StopWatch:
+    def __init__(self, task):
+        self.task = task
+        self.t0 = time.time()
+        logger.debug(f"[0.0s]task start: {task}")
+
+    def stop(self):
+        self.span = time.time() - self.t0
+        logger.debug(f"[{self.span:.1f}s]task stop: {self.task}")
+
+
+@contextmanager
+def context_time(task):
+    """Track time for a context using yield.
+
+    Usage:
+        with context_time("generating mesh") as sw:
+            # do your task
+        print(sw.span)
+    """
+    sw = StopWatch(task)
+    try:
+        # This is where the code block will execute
+        yield sw
+    finally:
+        sw.stop()
+
+
+def func_time(func):
+    """Decorator to get func exec time."""
+
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        with context_time(f"{func.__module__}.{func.__qualname__}"):
+            return func(*args, **kwargs)
+
+    return wrapper
+
+
+def run_cmd(cmd, check=True, text=True, cwd=None, **kwargs):
+    if isinstance(cmd, str):
+        cmdline = cmd
+        shell = True
+    else:
+        # ensure each arg is a string
+        cmd = [str(arg) for arg in cmd if arg]
+        cmdline = shlex.join(cmd)
+        shell = False
+
+    if cwd:
+        cmdline = f"cd {cwd} && {cmdline}"
+
+    with context_time(cmdline):
+        return subprocess.run(cmd, shell=shell, check=check, text=text, cwd=cwd, **kwargs)
+
+
+def get_cmd_output(cmd):
+    return run_cmd(cmd, capture_output=True).stdout.strip()
+
+
+def run_script(script_path, *args):
+    assert os.path.exists(script_path), f"script not found: {script_path}"
+    return run_cmd([script_path, *args])
+
+
+def ffmpeg_extract_audio(file: Path, ext: str = "opus") -> Path:
+    suffix = f".{ext}"
+    if file.suffix.lower() == suffix:
+        return file
+    script = f"scripts/ffmpeg_extract_{ext}.sh"
+    run_script(script, str(file), file.stem)
+    out_file = file.with_suffix(suffix)
+    assert out_file.exists()
+    return out_file
+
+
 class Transcriber:
     supported_formats = ["text", "txt"]
 
+    @func_time
     def load(self):
+        logger.info(f"{self.__class__.__name__} loading ...")
+        self._load()
+
+    def _load(self):
         # for time consuming operations, e.g. loading model, downloading, etc.
         pass
 
     def clean_file(self, file: str) -> str:
         # check file data type, ext, etc.
-        return file
+        return ffmpeg_extract_audio(file)
 
     def clean_format(self, format: str) -> bool:
         if self.supported_formats:
@@ -40,15 +125,13 @@ class Transcriber:
         # check supported langs, map lang code, etc.
         return language
 
+    @func_time
     def transcribe(self, file, language: str = None, format: str = "text") -> str:
         file = self.clean_file(file)
         language = self.clean_language(language)
         format = self.clean_format(format)
-        t0 = time.time()
-        text = self._transcribe(file, language=language, format=format)
-        t = time.time() - t0
-        logger.info(f"transcribing done in {t:.1f}s: \n{text}\n")
-        return text
+        with context_time(f"{self.__class__.__name__}._transcribe"):
+            return self._transcribe(file, language=language, format=format)
 
 
 class AssemblyAITranscriber(Transcriber):
@@ -144,18 +227,16 @@ class GLMASRTranscriber(Transcriber):
     model_id = "zai-org/GLM-ASR-Nano-2512"
     max_new_tokens = 500
 
-    def load(self):
+    def _load(self):
         from transformers import AutoModelForSeq2SeqLM, AutoProcessor
         logger.info(f"Loading model {self.model_id}...")
-        t0 = time.time()
         self.processor = AutoProcessor.from_pretrained(self.model_id)
         self.model = AutoModelForSeq2SeqLM.from_pretrained(self.model_id, dtype="auto", device_map="auto")
-        t = time.time() - t0
-        logger.info(f"Model loaded in {t:.1f}s")
 
-    def _transcribe(self, file, language: str = None, format: str = "text"):
+    def _transcribe(self, file: str|Path, language: str = None, format: str = "text"):
+        # TODO: Audio duration (2983.4s) exceeds 655s; truncating to first 655s.
         # audio: path, ndarray, torch.Tensor
-        inputs = self.processor.apply_transcription_request(file)
+        inputs = self.processor.apply_transcription_request(str(file))
         inputs = inputs.to(self.model.device, dtype=self.model.dtype)
         outputs = self.model.generate(
             **inputs,
@@ -170,7 +251,7 @@ class WhisperTranscriber(Transcriber):
     supported_formats = ["json", "text", "srt"]
     model_size = os.getenv("WHISPER_MODEL", "turbo")
 
-    def load(self):
+    def _load(self):
         import whisper
         self.model = whisper.load_model(self.model_size)
 
@@ -198,7 +279,7 @@ class WhisperTranscriber(Transcriber):
 
 class WhisperCPUTranscriber(WhisperTranscriber):
 
-    def load(self):
+    def _load(self):
         from whisper import Whisper
         self.model = Whisper(
             model=os.getenv("WHISPER_MODEL", "small"),
@@ -218,7 +299,7 @@ class FasterWhisperTranscriber(Transcriber):
     best_of = 5
     model_size = os.getenv("FASTER_WHISPER_MODEL_SIZE", "large-v3")
 
-    def load(self):
+    def _load(self):
         from faster_whisper import WhisperModel, BatchedInferencePipeline
         self.model = WhisperModel(self.model_size, device=self.device, compute_type=self.compute_type)
         self.pipeline = BatchedInferencePipeline(model=self.model)
