44fc17a9547fce90d27b4622cdde244cf134d780
guoqiao <guoqiao@gmail.com>
2026-01-02 15:11:38 +1300

    server.py: save uploadfile obj to tmp dir

--- server.py
+++ server.py
@@ -2,15 +2,16 @@
 """GLM-ASR FastAPI server for audio transcription."""
 
 import os
+import tempfile
+import typing
+from pathlib import Path
 from contextlib import asynccontextmanager
-from typing import Optional, Annotated
 
 from loguru import logger
 import torch
-import librosa
-from fastapi import FastAPI, Form, File, UploadFile, HTTPException
+from fastapi import FastAPI, Form, File, UploadFile
 
-from transcribers import get_transcriber
+from transcribers import get_transcriber, func_time, context_time
 
 transcriber = get_transcriber()
 
@@ -25,21 +26,33 @@ async def lifespan(app: FastAPI) -> None:
 app = FastAPI(lifespan=lifespan)
 
 
+@func_time
+def save_uploadfile(uploadfile: UploadFile) -> str:
+    temp_dir = Path(tempfile.mkdtemp())
+    temp_file = temp_dir / uploadfile.filename
+    temp_file.write_bytes(uploadfile.file.read())
+    logger.info(f"upload file saved to {temp_file}")
+    return temp_file
+
+
 @app.post("/v1/audio/transcriptions")
 async def transcribe(
     file: UploadFile = File(...),
-    model: Annotated[str, Form()] = "whisper-1",  # placeholder
-    language: Annotated[str, Form()] = None,
-    response_format: Annotated[str, Form()] = "text",  # text, srt, vtt
+    model: typing.Annotated[str, Form()] = "whisper-1",  # placeholder
+    language: typing.Annotated[str, Form()] = None,
+    response_format: typing.Annotated[str, Form()] = "text",  # text, srt, vtt
 ):
     """Transcribe audio file to text."""
-    logger.info(f"{transcriber.__class__.__name__} transcribing {file.filename} -> language {language} format {response_format}")
+    logger.info(f"{transcriber.__class__.__name__} transcribing {type(file)} {file.filename} -> language {language} format {response_format}")
 
     # load can accept a file-like object
     # file.file: SpooledTemporaryFile
-    audio_ndarray, sr = librosa.load(file.file, sr=16000)
+    # supported formats: wav|mp3|flac|ogg
+    # sr=None to keep orig sample rate
+    # audio_ndarray, sr = librosa.load(file.file, sr=16000)
+    file_path = save_uploadfile(file)
     # path/ndarray/torch.Tensor
-    return transcriber.transcribe(audio_ndarray, language=language, format=response_format)
+    return transcriber.transcribe(file_path, language=language, format=response_format)
 
 
 if __name__ == "__main__":
